71f7ea0325c6b288eee6ba70c328e210c27f7514
Xavier Cadena <xacadena@gmail.com>
2019-03-07 15:55:46 -0600

    Update fuzzer.cpp

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -1,7 +1,7 @@
 #include <folly/json_pointer.h>
 
 
-extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t Size) {
+extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
 
 //folly::StringPiece bin1(data, size);
 //folly::json_pointer::parse(bin1);
