edbad104bd34f51564855fba7dcfff6555eab676
Xavier Cadena <xacadena@gmail.com>
2019-03-13 21:15:14 -0500

    Update fuzzer.cpp

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -3,9 +3,8 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
 
-//folly::StringPiece bin1(data, size);
-//folly::json_pointer::parse(bin1);
+folly::StringPiece bin1(data, size);
   
-  folly::json_pointer::parse(data);
+  folly::json_pointer::parse(bin1);
         return 0;
 }
